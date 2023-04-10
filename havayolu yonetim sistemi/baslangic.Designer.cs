2d10abf5c7e44765418a5f2a1b2802cfeadb5db0
esra kabaş <115526819+esrakabas@users.noreply.github.com>
2023-04-10 03:53:36 +0300

    baslangic formu hazirlandi ve kodu yazildi

---
+++ havayolu yonetim sistemi/baslangic.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace havayolu_yonetim_sistemi
+{
+    partial class baslangic
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(baslangic));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.ŞAFAKJET = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(549, 328);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Maroon;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.progressBar1.Location = new System.Drawing.Point(0, 325);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(549, 15);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // ŞAFAKJET
+            // 
+            this.ŞAFAKJET.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ŞAFAKJET.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ŞAFAKJET.Font = new System.Drawing.Font("Palatino Linotype", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ŞAFAKJET.Location = new System.Drawing.Point(12, 201);
+            this.ŞAFAKJET.Name = "ŞAFAKJET";
+            this.ŞAFAKJET.Size = new System.Drawing.Size(181, 38);
+            this.ŞAFAKJET.TabIndex = 2;
+            this.ŞAFAKJET.Text = "ŞAFAKJET";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // baslangic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(550, 340);
+            this.Controls.Add(this.ŞAFAKJET);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "baslangic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label ŞAFAKJET;
+        private System.Windows.Forms.Timer timer1;
+        public System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ havayolu yonetim sistemi/baslangic.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace havayolu_yonetim_sistemi
+{
+    public partial class baslangic : Form
+    {
+        public baslangic()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        //prograssbar aracı ve timer kullanildi eger 100e esitse timer duracak ve giris formuna gecilecek
+
+        int basla = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            basla += 1;
+            progressBar1.Value = basla;
+            if(progressBar1.Value==100)
+            {
+                progressBar1.Value = 0;
+                timer1.Stop();
+                giris gr = new giris();
+                gr.Show();
+                this.Hide();
+            }
+        }
+    }
+}
