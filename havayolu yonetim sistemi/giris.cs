903607de52edb351db74f1708a904a83f21ef1bd
esrakabas <esrakabas.mdbf19@iste.edu.tr>
2023-04-12 03:24:19 +0300

    veritabanina baglama kodu yazildi

--- havayolu yonetim sistemi/giris.cs
+++ havayolu yonetim sistemi/giris.cs
@@ -5,16 +5,29 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Data.Sql;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace havayolu_yonetim_sistemi
 {
     public partial class giris : Form
     {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+
+
         public giris()
-        {
+        {  
+            //veritabanina bağlama
             InitializeComponent();
+            con.ConnectionString = @"Data Source=DESKTOP-MDN807P;Initial Catalog=havayolu;Integrated Security=True";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
